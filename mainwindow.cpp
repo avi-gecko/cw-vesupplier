7ae6ad4cc878a38eb0bb561905b3690a3d511c05
Алексей Ильин <thealexsnare@yandex.ru>
2023-03-20 13:56:44 +0300

    Добавлено первичное открытие файлаи генерация заголовков таблицы

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,11 @@
 #include "ui_mainwindow.h"
 
 #include <QMessageBox>
+#include <QFileDialog>
+#include <QString>
+#include <QFile>
+#include <QTextStream>
+#include <QStringList>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -19,5 +24,42 @@ MainWindow::~MainWindow()
 void MainWindow::about()
 {
     QMessageBox::about(this, QString(tr("О программе")), QString(tr("Создано Алексеем Ильиным ИЦТМС 2-5")));
+}
+
+
+void MainWindow::on_addButton_clicked()
+{
+    QString file_name = QFileDialog::getOpenFileName();
+    QFile file(file_name);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+
+    QTextStream in(&file);
+    QString header;
+    QStringList labels;
+    header = in.readLine();
+    if (header.startsWith(QString("<DB;")) and header.endsWith(QString(">")))
+    {
+        header = header.sliced(4, header.size() - 5);
+        labels = header.split(";");
+    }
+    else
+    {
+
+        QMessageBox::critical(  this
+                              , QString(tr("Ошибка!"))
+                              , QString(tr("Неверный формат файла.")));
+        file.close();
+        return;
+    }
+    ui->textEdit->setText(header);
+
+    ui->tableWidget->setColumnCount(labels.size());
+    ui->tableWidget->setHorizontalHeaderLabels(labels);
+  /*  while (!in.atEnd())
+    {
+         result += in.readLine();
+    }*/
+   file.close();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -20,5 +20,6 @@ private:
 
 private slots:
     void about();
+    void on_addButton_clicked();
 };
 #endif // MAINWINDOW_H

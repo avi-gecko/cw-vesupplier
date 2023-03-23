cce6d0815e1eb48c5b41d61786b98ddf746f82ea
Алексей Ильин <thealexsnare@yandex.ru>
2023-03-23 13:41:46 +0300

    Добавлены геттеры и сеттеры для класса вент. оборудования

--- vesupplier.cpp
+++ vesupplier.cpp
@@ -5,7 +5,91 @@ Vesupplier::Vesupplier()
 
 }
 
-Vesupplier::Vesupplier(QString nameOrganzation, QString OGRN, QString address, QString nameOwner, QString phone, unsigned int count, unsigned int price)
+Vesupplier::Vesupplier(QString nameOrganzation
+                     , QString OGRN
+                     , QString address
+                     , QString nameOwner
+                     , QString phone
+                     , unsigned int count
+                     , unsigned double price)
+
+                     : m_nameOrganizaton(nameOrganzation)
+                     , m_OGRN(OGRN)
+                     , m_address(address)
+                     , m_nameOwner(nameOwner)
+                     , m_phone(phone)
+                     , m_count(count)
+                     , m_price(price)
 {
 
+}
+
+void Vesupplier::setNameOrganization(const QString nameOrganizaton)
+{
+    m_nameOrganizaton = nameOrganizaton;
+}
+
+void Vesupplier::setOGRN(const QString OGRN)
+{
+    m_OGRN = OGRN;
+}
+
+void Vesupplier::setAddress(const QString address)
+{
+    m_address = address;
+}
+
+void Vesupplier::setNameOwner(const QString nameOwner)
+{
+    m_nameOwner = nameOwner;
+}
+
+void Vesupplier::setPhone(const QString phone)
+{
+    m_phone = phone;
+}
+
+void Vesupplier::setCount(const unsigned int count)
+{
+    m_count = count;
+}
+
+void Vesupplier::setPrice(const double price)
+{
+    m_price = price;
+}
+
+QString Vesupplier::getNameOrganization() const
+{
+    return m_nameOrganizaton;
+}
+
+QString Vesupplier::getOGRN() const
+{
+    return m_OGRN;
+}
+
+QString Vesupplier::getAddress() const
+{
+    return m_address;
+}
+
+QString Vesupplier::getNameOwner() const
+{
+    return m_nameOwner;
+}
+
+QString Vesupplier::getPhone() const
+{
+    return m_phone;
+}
+
+unsigned int Vesupplier::getCount() const
+{
+    return m_count;
+}
+
+double Vesupplier::getPrice() const
+{
+    return m_price;
 }
--- vesupplier.h
+++ vesupplier.h
@@ -1,11 +1,45 @@
 #ifndef VESUPPLIER_H
 #define VESUPPLIER_H
-
+#include <QString>
 
 class Vesupplier
 {
 public:
     Vesupplier();
+    Vesupplier(QString nameOrganzation
+             , QString OGRN
+             , QString address
+             , QString nameOwner
+             , QString phone
+             , unsigned int count
+             , unsigned double price);
+
+    void setNameOrganization(const QString nameOrganizaton);
+    void setOGRN(const QString OGRN);
+    void setAddress(const QString address);
+    void setNameOwner(const QString nameOwner);
+    void setPhone(const QString phone);
+    void setCount(const unsigned int count);
+    void setPrice(const unsigned double price);
+
+    QString getNameOrganization() const;
+    QString getOGRN() const;
+    QString getAddress() const;
+    QString getNameOwner() const;
+    QString getPhone() const;
+    unsigned int getCount() const;
+    unsigned double getPrice() const;
+
+private:
+    Vesupplier(const Vesupplier&) = delete;
+
+    QString m_nameOrganizaton;
+    QString m_OGRN;
+    QString m_address;
+    QString m_nameOwner;
+    QString m_phone;
+    unsigned int m_count;
+    unsigned int m_price;
 };
 
 #endif // VESUPPLIER_H

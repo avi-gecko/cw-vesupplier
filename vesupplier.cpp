06669a799c900b4814b8ad9767913a66a8d9af7a
Алексей Ильин <thealexsnare@yandex.ru>
2023-03-23 13:23:41 +0300

    Добавлен конструктор класса вент. оборудования

--- vesupplier.cpp
+++ vesupplier.cpp
@@ -4,3 +4,8 @@ Vesupplier::Vesupplier()
 {
 
 }
+
+Vesupplier::Vesupplier(QString nameOrganzation, QString OGRN, QString address, QString nameOwner, QString phone, unsigned int count, unsigned int price)
+{
+
+}

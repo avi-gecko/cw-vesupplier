237d45772a4ce42af1a804e59e7de07c2d94fa6e
Алексей Ильин <thealexsnare@yandex.ru>
2023-04-04 20:32:34 +0300

    Добавлены переводы

--- cw-vesupplier_ru_RU.ts
+++ cw-vesupplier_ru_RU.ts
@@ -4,9 +4,97 @@
 <context>
     <name>AddDialog</name>
     <message>
-        <location filename="adddialog.ui" line="13"/>
-        <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <location filename="adddialog.ui" line="14"/>
+        <source>Add row</source>
+        <translation>Добавить запись</translation>
+    </message>
+    <message>
+        <location filename="adddialog.ui" line="20"/>
+        <source>Name</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location filename="adddialog.ui" line="30"/>
+        <source>OGRN</source>
+        <translation>ОГРН</translation>
+    </message>
+    <message>
+        <location filename="adddialog.ui" line="40"/>
+        <source>Address</source>
+        <translation>Адрес</translation>
+    </message>
+    <message>
+        <location filename="adddialog.ui" line="50"/>
+        <source>Owner name</source>
+        <translation>Имя владельца</translation>
+    </message>
+    <message>
+        <location filename="adddialog.ui" line="60"/>
+        <source>Phone</source>
+        <translation>Номер телефона</translation>
+    </message>
+    <message>
+        <location filename="adddialog.ui" line="70"/>
+        <source>Product count</source>
+        <translation>Количество товара</translation>
+    </message>
+    <message>
+        <location filename="adddialog.ui" line="80"/>
+        <source>Post count</source>
+        <translation>Количество поставок</translation>
+    </message>
+    <message>
+        <location filename="adddialog.ui" line="90"/>
+        <source>Price</source>
+        <translation>Цена</translation>
+    </message>
+</context>
+<context>
+    <name>EditDialog</name>
+    <message>
+        <location filename="editdialog.ui" line="14"/>
+        <source>Edit row</source>
+        <translation>Изменить запись</translation>
+    </message>
+    <message>
+        <location filename="editdialog.ui" line="20"/>
+        <source>Name</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location filename="editdialog.ui" line="30"/>
+        <source>OGRN</source>
+        <translation>ОГРН</translation>
+    </message>
+    <message>
+        <location filename="editdialog.ui" line="40"/>
+        <source>Address</source>
+        <translation>Адрес</translation>
+    </message>
+    <message>
+        <location filename="editdialog.ui" line="50"/>
+        <source>Owner name</source>
+        <translation>Имя владельца</translation>
+    </message>
+    <message>
+        <location filename="editdialog.ui" line="60"/>
+        <source>Phone</source>
+        <translation>Номер телефона</translation>
+    </message>
+    <message>
+        <location filename="editdialog.ui" line="70"/>
+        <source>Product count</source>
+        <translation>Количество товара</translation>
+    </message>
+    <message>
+        <location filename="editdialog.ui" line="80"/>
+        <source>Post count</source>
+        <translation>Количество поставок</translation>
+    </message>
+    <message>
+        <location filename="editdialog.ui" line="90"/>
+        <source>Price</source>
+        <translation>Цена</translation>
     </message>
 </context>
 <context>
@@ -52,42 +140,97 @@
         <translation>Файл</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="110"/>
-        <location filename="mainwindow.cpp" line="35"/>
+        <location filename="mainwindow.ui" line="111"/>
+        <location filename="mainwindow.cpp" line="38"/>
         <source>About</source>
         <translation>О программе</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="119"/>
+        <location filename="mainwindow.ui" line="120"/>
         <source>Open</source>
         <translation>Открыть</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="124"/>
+        <location filename="mainwindow.ui" line="125"/>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="129"/>
+        <location filename="mainwindow.ui" line="130"/>
         <source>Exit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="36"/>
+        <location filename="mainwindow.ui" line="135"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="39"/>
         <source>Created by Alexey Ilin ICTMS 2-5</source>
         <translation>Создано Алексеем Ильином ИЦТМС 2-5</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="56"/>
+        <location filename="mainwindow.cpp" line="46"/>
+        <source>Open file</source>
+        <translation>Открытие файла</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="48"/>
+        <source>Database (*.db)</source>
+        <translation>База данных</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="62"/>
         <source>Error!</source>
         <translation>Ошибка!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="110"/>
+        <location filename="mainwindow.cpp" line="121"/>
+        <location filename="mainwindow.cpp" line="160"/>
+        <location filename="mainwindow.cpp" line="177"/>
+        <location filename="mainwindow.cpp" line="198"/>
+        <source>Warning!</source>
+        <translation>Внимание!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="111"/>
+        <location filename="mainwindow.cpp" line="122"/>
+        <source>You should open document before saving.</source>
+        <translation>Вам следует открыть документ перед сохранением.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="148"/>
+        <source>Success!</source>
+        <translation>Успех!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="149"/>
+        <source>File is successfuly saved.</source>
+        <translation>Файл успешно сохранён.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="161"/>
+        <source>You should open document before adding.</source>
+        <translation>Вам следует открыть документ перед добавлением записи.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="178"/>
+        <source>You should open document before deleting.</source>
+        <translation>Вам следует открыть документ перед удалением записи.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="199"/>
+        <source>You should open document before editing.</source>
+        <translation>Вам следует открыть документ перед редактированием записи.</translation>
     </message>
     <message>
         <source>Table can&apos;t has columns.</source>
         <translation type="vanished">Таблица не может не иметь столбцов.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="57"/>
+        <location filename="mainwindow.cpp" line="63"/>
         <source>Wrong file format.</source>
         <translation>Неверный формат файла.</translation>
     </message>
@@ -95,42 +238,46 @@
 <context>
     <name>VesupplierTableModel</name>
     <message>
-        <location filename="vesuppliertablemodel.cpp" line="90"/>
+        <location filename="vesuppliertablemodel.cpp" line="92"/>
         <source>Name</source>
         <translation>Название</translation>
     </message>
     <message>
-        <location filename="vesuppliertablemodel.cpp" line="92"/>
+        <location filename="vesuppliertablemodel.cpp" line="94"/>
         <source>OGRN</source>
         <translation>ОГРН</translation>
     </message>
     <message>
-        <location filename="vesuppliertablemodel.cpp" line="94"/>
+        <location filename="vesuppliertablemodel.cpp" line="96"/>
         <source>Address</source>
         <translation>Адрес</translation>
     </message>
     <message>
-        <location filename="vesuppliertablemodel.cpp" line="96"/>
         <source>Name owner</source>
-        <translation>ФИО владельца</translation>
+        <translation type="vanished">ФИО владельца</translation>
     </message>
     <message>
         <location filename="vesuppliertablemodel.cpp" line="98"/>
+        <source>Owner name</source>
+        <translation>Имя владельца</translation>
+    </message>
+    <message>
+        <location filename="vesuppliertablemodel.cpp" line="100"/>
         <source>Phone</source>
         <translation>Номер телефона</translation>
     </message>
     <message>
-        <location filename="vesuppliertablemodel.cpp" line="100"/>
+        <location filename="vesuppliertablemodel.cpp" line="102"/>
         <source>Product count</source>
         <translation>Количество товара</translation>
     </message>
     <message>
-        <location filename="vesuppliertablemodel.cpp" line="102"/>
+        <location filename="vesuppliertablemodel.cpp" line="104"/>
         <source>Post count</source>
         <translation>Количество поставок</translation>
     </message>
     <message>
-        <location filename="vesuppliertablemodel.cpp" line="104"/>
+        <location filename="vesuppliertablemodel.cpp" line="106"/>
         <source>Price</source>
         <translation>Цена</translation>
     </message>
